b736f94a923b326819569a04fb8f9f1b546bd6b0
Edgar Briandt <99098876+Merxibeaucoup@users.noreply.github.com>
2023-01-05 22:42:23 -0500

    first com

--- src/main/java/com/edgar/security/auth/AuthenticationService.java
+++ src/main/java/com/edgar/security/auth/AuthenticationService.java
@@ -1,5 +1,32 @@
 package com.edgar.security.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import com.edgar.security.repository.AppUserRepository;
+import com.edgar.security.user.AppUser;
+import com.edgar.security.user.AppUserBuilder;
+
+@Service
 public class AuthenticationService {
+	
+	@Autowired
+	AppUserRepository repository;
+
+	public AuthenticationResponse register(RegisterRequest request) {
+			var user = AppUserBuilder.build()
+					.firstname(request.getFirstname())
+					.lastname(request.getLastname())
+					.email
+					
+					.build();
+		return null;
+	}
+
+	public AuthenticationResponse authenticate(AuthenticationRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
